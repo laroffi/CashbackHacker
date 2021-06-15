63cc35cf54df93d316640c4aa9339b031c1d71cf
Larisa Pylaeva <laralord@yandex.ru>
2021-06-16 00:28:38 +0300

    junit4 test run

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,40 +1,40 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    void shouldShowIfUnder1000() {
+    public void shouldShowIfUnder1000() {
         int amount = 890;
 
         int actual = service.remain(amount);
         int expected = 110;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldShowIfOver1000() {
+   public void shouldShowIfOver1000() {
         int amount = 1733;
 
         int actual = service.remain(amount);
         int expected = 267;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldShowIfEqual1000() {
+    public void shouldShowIfEqual1000() {
         int amount = 1000;
 
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 
